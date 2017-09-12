d27035066825b8b97353e973a06a58d9bed2b8fd
Wah Yuen <wah.yuen@lateral.com.au>
2017-09-12 14:08:05 +0800

    removing redundant silent auth endpoint

--- auth0api/Controllers/PingController.cs
+++ auth0api/Controllers/PingController.cs
@@ -1,7 +1,3 @@
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,27 +16,9 @@ namespace auth0api.Controllers
         [Authorize]
         [HttpGet]
         [Route("ping/secure")]
-        public async Task<JsonResult> PingSecured()
+        public JsonResult PingSecured()
         {
-            var user = HttpContext.User;
-
-            // The user's ID is available in the NameIdentifier claim
-            string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-            var authenticateInfo = await HttpContext.AuthenticateAsync("Bearer");
-            string accessToken = authenticateInfo.Properties.Items[".Token.access_token"];
             return new JsonResult("All good. You only get this message if you are authenticated.");
-        }
-
-        [Authorize]
-        [HttpGet("claims")]
-        public object Claims()
-        {
-            return User.Claims.Select(c =>
-            new
-            {
-                Type = c.Type,
-                Value = c.Value
-            });
         }
     }
 }
